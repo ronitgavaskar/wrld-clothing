7830cae8162a45ac7ee4173e50c72bee6a2056d4
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-19 15:54:43 -0700

    Created barebone skeleton of my ecommerce website

---
+++ src/homepage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import './homepage.scss'
+
+const Homepage = () => {
+    return(
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <h1 className="subtitle">SHOP NOW</h1>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <h1 className="subtitle">SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMENS</h1>
+                        <h1 className="subtitle">SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MENS</h1>
+                        <h1 className="subtitle">SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SNEAKERS</h1>
+                        <h1 className="subtitle">SHOP NOW</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Homepage;
